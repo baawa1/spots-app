de1d9ba690cfd272a8eecf0f05b22e6c9b3d53d0
BaaWA <bmdebell32@gmsil.com>
2025-05-19 11:16:35 +0100

    The divider was rebuilt

--- src/App.jsx
+++ src/App.jsx
@@ -49,39 +49,45 @@ function App() {
           </nav>
         </header>
         <main className=" bg-[#FCF5E5]">
-          <section className="max-w-7xl mx-auto pb-5 pt-[34px] px-8 border-b border-[#21212166] border-opacity-[40%]">
-            <div className=" flex md:flex-row flex-col items-center md:justify-between gap-8">
-              <div className="flex md:flex-row flex-col justify-center items-center md:items-stretch text-center md:text-left gap-x-5">
-                <img
-                  src={avatar}
-                  alt="Bessie Coleman"
-                  className="md:w-[190px] w-full"
-                />
+          <section className="relative max-w-7xl mx-auto  pt-[34px] px-8">
+            <div className="relative pb-5">
+              <div className="flex md:flex-row flex-col items-center md:justify-between gap-8">
+                <div className="flex md:flex-row flex-col justify-center items-center md:items-stretch text-center md:text-left gap-x-5">
+                  <img
+                    src={avatar}
+                    alt="Bessie Coleman"
+                    className="md:w-[190px] w-full"
+                  />
 
-                <div className="flex flex-col justify-between">
-                  <div>
-                    <h1 className=" text-4xl font-medium">Bessie Coleman</h1>
-                    <p>Civil Aviator</p>
+                  <div className="flex flex-col justify-between">
+                    <div>
+                      <h1 className=" text-4xl font-medium">Bessie Coleman</h1>
+                      <p>Civil Aviator</p>
+                    </div>
+                    <button className=" flex gap-2 items-center md:justify-normal justify-center mt-8">
+                      <img src={editIcon} alt="Edit icon button" />
+                      <span>Edit Profile</span>
+                    </button>
                   </div>
-                  <button className=" flex gap-2 items-center md:justify-normal justify-center mt-8">
-                    <img src={editIcon} alt="Edit icon button" />
-                    <span>Edit Profile</span>
+                </div>
+                <div className="flex align-bottom md:self-end w-full md:w-44 justify-end">
+                  <button className="bg-[#212121] text-white flex flex-row justify-center gap-2 py-4 rounded-lg md:w-40 w-full">
+                    <img src={newIcon} alt="" />
+                    <span>New Produc</span>
                   </button>
                 </div>
               </div>
-              <div className="flex align-bottom md:self-end w-full md:w-44 justify-end">
-                <button className="bg-[#212121] text-white flex flex-row justify-center gap-2 py-4 rounded-lg md:w-40 w-full">
-                  <img src={newIcon} alt="" />
-                  <span>New Produc</span>
-                </button>
-              </div>
+              <div className="absolute bottom-0 w-full border-b border-[#21212166] border-opacity-[40%]"></div>
             </div>
           </section>
-          <section className="max-w-7xl mx-auto pt-2.5 pb-10 px-8 border-b border-[#21212166] border-opacity-[40%]">
-            <div className=" grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-x-5 gap-y-10">
-              {spotsContents.map(({ key, title, img }) => (
-                <GridImage title={title} img={img} key={key} />
-              ))}
+          <section className="max-w-7xl mx-auto pt-2.5 px-8">
+            <div className="relative pb-10">
+              <div className=" grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-x-5 gap-y-10">
+                {spotsContents.map(({ key, title, img }) => (
+                  <GridImage title={title} img={img} key={key} />
+                ))}
+              </div>
+              <div className="absolute bottom-0 w-full max-w-[76rem] border-b border-[#21212166] border-opacity-[40%]"></div>
             </div>
           </section>
           <footer className="h-32 flex items-center justify-center">
